a3ca7e3c0fb91d0be0ee6d3646b249d69da5534d
sumin <dwckey5356@gmail.com>
2025-02-27 04:48:25 -0700

    Initial Commit

---
+++ testinghandpring/testinghandpringApp.swift
@@ -0,0 +1,17 @@
+//
+//  testinghandpringApp.swift
+//  testinghandpring
+//
+//  Created by 김수민 on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct testinghandpringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testinghandpringTests/testinghandpringTests.swift
@@ -0,0 +1,17 @@
+//
+//  testinghandpringTests.swift
+//  testinghandpringTests
+//
+//  Created by 김수민 on 2/27/25.
+//
+
+import Testing
+@testable import testinghandpring
+
+struct testinghandpringTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
